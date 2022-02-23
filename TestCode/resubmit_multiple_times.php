a9c8b9c422f70cac3500c4e63bee0f333bda04c2
Noah Padilla <npizza10@gmail.com>
2022-02-22 19:25:31 -0700

    added  functionality to add multiple items and added back color

--- TestCode/resubmit_multiple_times.php
+++ TestCode/resubmit_multiple_times.php
@@ -1,1 +1,16 @@
 <?php
+session_start();
+
+if (!isset($_SESSION['count']))
+{
+    $_SESSION['count'] = 1;
+}
+else
+{
+    echo session_id();
+    echo '<br>';
+    ++$_SESSION['count'];
+}
+
+echo $_SESSION['count'];
+?>
--- lab1_PLU_System/main.php
+++ lab1_PLU_System/main.php
@@ -1,8 +1,18 @@
+
+<!--
+TODO:
+    - Add more than 1 item to list
+    - Allow to update alias
+    - Interesting feature
+
+
+
+-->
 <html lang="en">
     <head>
         <title>PHP Test</title>
     </head>
-    <body>
+    <body style="background-color:grey;">
             <h1>PLU System</h1>
             <h3>Enter a product and it's PLU to be stored in the system</h3>
 
@@ -16,39 +26,47 @@
                 Product Icon: <input type='file' name='picon' /><br><br>
 
                 Password: <input type='text' name='pw' /><br><br>
-                <input type='submit' name='submit' value='submit' />
+                <input type='submit' name='submit' value='Submit' />
             </form>
             <!-- Show the list below where each product is an object -->
             <?php
             include("product.php");
-            $inventory = array();
 
-            // Submit user input data for 2D array
-            if ( isset($_POST['submit']) && $_POST['pw'] == 'pw') {
-                
+            //writes the object to a file by appending it
+            function writeObject($obj) {
+                $objData = serialize($obj);
+                $filePath = getcwd().DIRECTORY_SEPARATOR."inventory.txt";
+                $fp = fopen($filePath, "a");
+                fwrite($fp, $objData . "\n");
+                fclose($fp);
+            }
 
-                $name = $_POST['name'];
-                $alias = $_POST['alias'];
-                $plu = $_POST['plu'];
-                $picon = $_POST['picon']; // THE SELECTED IMAGE HAS TO BE WITHIN SAME DIRECTORY AS THIS PROJECT
+            //writes the object to a file by appending it
+            function readObjects() {
 
-                //echo gettype($picon) . "$picon <br>";
+                $filePath = getcwd().DIRECTORY_SEPARATOR."inventory.txt";
+                $objData = file_get_contents($filePath);
+                $all_objs = explode("\n", $objData);
 
-                array_push($inventory, new product($name, $alias, $plu,$picon));
+                $inventory = array();
+                for($i = 0; $i < count($all_objs) - 1; $i++) {
+                    //convert object in string format to object format and push it into the inventory array show all for listing
+                    array_push($inventory, unserialize($all_objs[$i]));
+                }
 
-                //print_r($inventory);
+
                 //Display all the inventory in a table fashion
                 echo"<br>";
                 echo "<table border='2'>
-              <thead>
-                <tr>
-                <th>Name</th>
-                <th style = 'background-color: #009933'>Alias</th>
-                <th>PLU</th>
-                <th>Picture</th>
-                </tr>
-              </thead>
-              ";
+                  <thead>
+                    <tr>
+                    <th>Name</th>
+                    <th style = 'background-color: #009933'>Alias</th>
+                    <th>PLU</th>
+                    <th>Picture</th>
+                    </tr>
+                  </thead>
+                  ";
                 foreach ($inventory as $item) {
                     echo '<tr>';
                     echo '<td>' . $item->name . '</td>';
@@ -57,10 +75,29 @@
                     echo '<td><img alt="product image" height=100 width=100 src=".\/'. $item->picon.'"></td>';
                     echo '</tr>';
                 }
+
+
             }
-            if ( !empty($_POST['pw']) && $_POST['pw'] != 'pw'){
-                echo "** password was wrong, please try again **";
+
+
+            // Submit user input data for the inventory
+            if ( isset($_POST['submit']) && $_POST['pw'] == 'pw') {
+
+                //check if its in text file already
+
+
+                $name = $_POST['name'];
+                $alias = $_POST['alias'];
+                $plu = $_POST['plu'];
+                $picon = $_POST['picon']; // THE SELECTED IMAGE HAS TO BE WITHIN SAME DIRECTORY AS THIS PROJECT
+
+                writeObject(new product($name, $alias, $plu,$picon));
+                readObjects();
+
             }
+
+
+
 
             ?>
     </body
