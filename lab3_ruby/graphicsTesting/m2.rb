fc930cf5d23be92b80f80c4ec49836ffe8f0b60d
noah <npizza10@gmail.com>
2022-04-27 15:34:19 -0600

    added basic code to add to hash when printing game board

--- lab3_ruby/graphicsTesting/m2.rb
+++ lab3_ruby/graphicsTesting/m2.rb
@@ -1,5 +1,6 @@
 require 'ruby2d'
 require_relative 'level'
+require_relative 'board'
 
 # @down = false
 # level = Level.new
@@ -28,13 +29,31 @@ require_relative 'level'
 # level.show
 
 def draw_grid(level=nil)
-  @level = level || 0
   set( { title: 'Connect Four', background: 'white', height: 640, width: 640 } )
-  @grid = Grid.new(DOTS[@level]) #This is where the array will be
+  @grid = Grid.new(DOTS[0]) #This is where the array will be
   @line = []
   @tiles = @grid.tiles
 end
 
-draw_grid(0)
+
+# test some robot-player behaviors
+testboard1 = Board.new
+testboard1.addDisc(:R,4)
+testboard1.addDisc(:O,4)
+testboard1.addDisc(:R,5)
+testboard1.addDisc(:O,5)
+testboard1.addDisc(:R,6)
+testboard1.addDisc(:O,6)
+#testResult(:hwin, robotMove(:R, testboard1),[3], 'robot should take horizontal win')
+#testboard1.print
+#testboard1.popOut(4) #removes the bottom most counter on the 4th column (cols start at 0)
+#testboard1.print
+
+draw_grid()
+col = 0
+puts DOTS[0][:dots][:yellow]
+DOTS[0][:dots][:yellow] << 0 * 5 + col
+puts DOTS[0][:dots][:yellow]
+draw_grid()
 
 show
