d0119d07ffbe5267fe307a6133b6fc1708d05655
Noah Padilla <npizza10@gmail.com>
2022-04-28 12:57:47 -0600

    put the old board back to demo popout

--- lab3_ruby/main.rb
+++ lab3_ruby/main.rb
@@ -10,27 +10,19 @@ def draw_grid(color)
   @tiles = @grid.tiles
 end
 
-
-# test some robot-player behaviors
-# testboard1 = Board.new
-# testboard1.addDisc(:R,4)
-# testboard1.addDisc(:O,4)
-# testboard1.addDisc(:R,5)
-# testboard1.addDisc(:O,5)
-# testboard1.addDisc(:R,6)
-# testboard1.addDisc(:O,6)
-#testResult(:hwin, robotMove(:R, testboard1),[3], 'robot should take horizontal win')
-#testboard1.print
-#testboard1.popOut(4) #removes the bottom most counter on the 4th column (cols start at 0)
-#testboard1.print
-
-# draw_grid()
-# col = 0
-# puts DOTS[0][:dots][:yellow]
-# DOTS[0][:dots][:yellow] << 0 * 5 + col
-# puts DOTS[0][:dots][:yellow]
-# draw_grid()
-# show
+#-----------------------------------------------------------------------------
+testboard1 = Board.new
+testboard1.addDisc(:R,4)
+testboard1.addDisc(:O,4)
+testboard1.addDisc(:R,5)
+testboard1.addDisc(:O,5)
+testboard1.addDisc(:R,6)
+testboard1.addDisc(:O,6)
+puts "Printing before pop out function"
+testboard1.print
+testboard1.popOut(4) #removes the bottom most counter on the 4th column (cols start at 0)
+puts "Printing after pop out function"
+testboard1.print
 
 #---------------------------------------------------------------------------
 
