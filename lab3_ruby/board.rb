55b34eb0e34fc09428b7ce3be72863b1db779fe4
Noah Padilla <npizza10@gmail.com>
2022-04-28 13:30:38 -0600

    update the pop to start at bottom instead of at top

--- lab3_ruby/board.rb
+++ lab3_ruby/board.rb
@@ -36,8 +36,6 @@ class Board
 
   def print
     puts @board.map {|row| row.map { |e| e || " "}.join("|")}.join("\n") # <--------- Original
-    #puts @board.map {|row| row.reverse_each.map { |e| e || " "}.join("|")}.join("\n").reverse #my modification
-
     #below prints the area in the classic format
     # @board.each do |value|
     #     puts value.to_s + " "
@@ -72,12 +70,14 @@ class Board
 
   #------------------------------------------------------------------
   def popOut(col)
-    i = 0
+
+    i = 5
+    #puts "IN POP OUT #{i} #{col} #{@board[i][col]}"
     #board.print
     #puts "#{board.class}"
     while @board[i][col] != nil
-      @board[i][col] = @board[i+1][col]
-      i+=1
+      @board[i][col] = @board[i-1][col]
+      i-=1
     end
   end
 
